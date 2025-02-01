da088e5f197d1caadd724ac6e5a4c97a56831f49
Mohd Samir <mohdsamir30102001@gmail.com>
2025-02-01 22:29:44 +0530

    Know About MiddleWare

--- src/app.js
+++ src/app.js
@@ -1,30 +1,21 @@
 const express = require("express");
 const app = express();
 // request handler function
-app.use(
-  "/user",
-  [
-    (req, res, next) => {
-      console.log("Handling The Route User 1");
-      next();
-      // res.send("Response 1");
-    },
-    (req, res, next) => {
-      console.log("Handling The Route User 2");
-      // res.send("Response 2");
-      next();
-    },
-  ],
-  (req, res, next) => {
-    console.log("Handling The Route User 3");
-    // res.send("Response 3");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling The Route User 4");
-    res.send("Response 4");
-  }
-);
+// GET /users => middleware chain => request handlet
+app.use("/", (req, res, next) => {
+  // res.send("Handling / Route");
+  next();
+});
+app.get("/user", (req, res, next) => {
+  console.log("Handling the route user 1");
+  res.send("First route handler");
+  next();
+});
+app.get("/user", (req, res, next) => {
+  console.log("Handling the route user 2");
+  // res.send("Second route handler");
+  // next();
+});
 app.listen(7860, () => {
   console.log("Server is sucessfully listening on port 7860");
 });
