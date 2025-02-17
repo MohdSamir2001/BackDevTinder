7249eb232cdca953dfd78a5d85ed371927aac6e4
Mohd Samir <mohdsamir30102001@gmail.com>
2025-02-17 21:12:54 +0530

    Created Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequests");
 const userRouter = express.Router();
+const User = require("../models/user");
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 // get all the pending connections requests for the logged In User
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
@@ -41,6 +42,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       message: "Data fetched successfully",
       data,
     });
+  } catch (err) {
+    res.status(404).send("ERROR : " + err.message);
+  }
+});
+userRouter.get("/feed", userAuth, async (req, res) => {
+  // User should see all the user cards except
+  // 1) His own card
+  // 2) His connections
+  // 3) Ignored People
+  // 4) Already Sent to connection request to that person
+  try {
+    const loggedInUser = req.user;
+    // Find all connections requests (sent + received)
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id, // it gives requests that user sent
+        },
+        {
+          toUserId: loggedInUser._id, // it gives requests that user received
+        },
+      ],
+    })
+      .select("fromUserId toUserId")
+      .populate("fromUserId", "firstName")
+      .populate("toUserId", "firstName");
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId._id.toString());
+      hideUsersFromFeed.add(req.toUserId._id.toString());
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+    res.send(users);
   } catch (err) {
     res.status(404).send("ERROR : " + err.message);
   }
